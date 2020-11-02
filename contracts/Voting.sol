7e53ad2662dfb5edc938ca030106b312286a0cdc
Ivan Marchuk <ivan.marchuk@mail.ru>
2020-11-02 21:06:14 +0300

    added discard option

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,28 +1,52 @@
 pragma solidity >=0.5.0;
 
 contract Voting {
+    string public name;
+    string public description;
+
     struct Proposal {
-        uint256 id;
+        uint8 id;
+        string name;
+        string description;
+        uint256 voteCount;
+    }
+
+    struct Discard {
+        uint8 id;
         string name;
         uint256 voteCount;
     }
 
-    mapping(uint256 => Proposal) public proposals;
+    mapping(uint8 => Proposal) public proposals;
+    mapping(uint8 => Discard) public discards;
     mapping(address => bool) public voters;
 
-    uint256 public proposalsCount;
+    uint8 public proposalsCount;
 
+    // TODO: automatically init constructor: factory/voting ???
     constructor() public {
-        addProposal("White");
-        addProposal("Black");
+        addProposal("White", "wall color");
+        addProposal("Black", "wall color");
+        addDiscard("Discard");
     }
 
-    function addProposal(string memory _name) private {
+    function addProposal(string memory _name, string memory _description)
+        private
+    {
         proposalsCount++;
-        proposals[proposalsCount] = Proposal(proposalsCount, _name, 0);
+        proposals[proposalsCount] = Proposal(
+            proposalsCount,
+            _name,
+            _description,
+            0
+        );
     }
 
-    function castVote(uint8 _proposalId, uint32 votePower) public {
+    function addDiscard(string memory _name) private {
+        discards[0] = Discard(0, _name, 0);
+    }
+
+    function vote(uint8 _proposalId, uint16 votePower) public {
         // check that never voted
         require(!voters[msg.sender]);
 
@@ -33,8 +57,13 @@ contract Voting {
         voters[msg.sender] = true;
 
         // increase vote count for proposal
-        proposals[_proposalId].voteCount =
-            proposals[_proposalId].voteCount +
-            votePower;
+        proposals[_proposalId].voteCount += votePower;
+    }
+
+    function discard(uint16 votePower) public {
+        require(!voters[msg.sender]);
+        voters[msg.sender] = true;
+
+        discards[0].voteCount += votePower;
     }
 }
