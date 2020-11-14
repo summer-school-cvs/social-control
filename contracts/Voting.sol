ec88be25dab645889426707c0f3c1103c8efc4fb
VANADAIN <ussrivanko@gmail.com>
2020-11-14 21:20:31 +0300

    added decision struct, deleted Voter.voted state, added validPercent restriction to win

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -5,8 +5,10 @@ contract Voting {
 
     // restrictions
     bool public Ended;
-    uint256 public votingEndTime;
+    uint256 private votingEndTime;
     bool public authByOwner;
+    // defines a percent of votes to win
+    uint16 private validPercent;
 
     struct Proposal {
         uint8 id;
@@ -22,21 +24,27 @@ contract Voting {
     }
 
     struct Voter {
-        bool voted;
         uint16 votePower;
+    }
+    
+    struct Decision {
+        uint16 prevVotePower;
+        uint8 proposalId;
     }
 
     mapping(uint8 => Proposal) public proposals;
     mapping(uint8 => Discard) public discards;
     mapping(address => Voter) public voters;
+    mapping(address => Decision) public decisions;
 
     uint8 public proposalsCount;
 
-    constructor(uint256 _durationMinutes, bool _authByOwner) public {
+    constructor(uint16 _validPercent, uint256 _durationMinutes, bool _authByOwner) public {
         votingCreator = msg.sender;
         Ended = false;
         authByOwner = _authByOwner;
         votingEndTime = block.timestamp + (_durationMinutes * 1 minutes);
+        validPercent = _validPercent;
         addProposal("White", "wall color");
         addProposal("Black", "wall color");
         addDiscard("Discard");
@@ -60,14 +68,12 @@ contract Voting {
 
     function autoAuthorize() public {
         require(!authByOwner, "Auto-auth is prohibited by creator.");
-        voters[msg.sender].voted = false;
         // Default (not delegate)
         voters[msg.sender].votePower = 1;
     }
 
     function authorize(address _voterAdress) public {
         require(msg.sender == votingCreator);
-        voters[_voterAdress].voted = false;
         // Default (not delegate)
         voters[_voterAdress].votePower = 1;
     }
@@ -76,28 +82,37 @@ contract Voting {
         require(!Ended, "Voting was ended by creator.");
         require(block.timestamp < votingEndTime, "Voting is over.");
 
-        // check that voter had never voted
-        require(!voters[msg.sender].voted, "Voter already voted.");
-
         // only valid id of proposal
         require(
             _proposalId > 0 && _proposalId <= proposalsCount,
             "Incorrect ID of the proposal."
         );
 
-        // record that address has voted
-        voters[msg.sender].voted = true;
+        // check if user has a previous decision
+        if (decisions[msg.sender]) {
+            // change proposal count with previous decision (return to state without sender adress vote)
+            proposals[decisions[msg.sender].proposalId].voteCount -= decisions[msg.sender].prevVotePower;
 
-        // increase vote count for proposal
-        proposals[_proposalId].voteCount += voters[msg.sender].votePower;
+            // write new decision
+            decisions[msg.sender].proposalId = _proposalId
+
+            // vote power in decision becomes a previous for next vote by adress
+            // current vote power may be different
+            decisions[msg.sender].prevVotePower = voters[msg.sender].votePower
+            
+            // now, when we wrote current vote power -> add it to proposal count
+            proposals[_proposalId].voteCount += voters[msg.sender].votePower;
+        } else {
+            // voting first time
+            decisions[msg.sender].prevVotePower = voters[msg.sender].votePower
+            decisions[msg.sender].proposalId = _proposalId
+            proposals[_proposalId].voteCount += voters[msg.sender].votePower;
+        }
     }
 
     function discard() public {
         require(!Ended, "Voting was ended by creator.");
         require(block.timestamp < votingEndTime, "Voting is over.");
-        require(!voters[msg.sender].voted, "Voter already voted.");
-
-        voters[msg.sender].voted = true;
 
         discards[0].voteCount += voters[msg.sender].votePower;
     }
