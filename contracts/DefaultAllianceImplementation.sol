b572bdadddeae77da3d6ce76750b93927a1d26f0
Ivan Marchuk <ivan.marchuk@mail.ru>
2020-11-25 21:25:55 +0300

    feat: updated election.sol

--- contracts/DefaultAllianceImplementation.sol
+++ contracts/DefaultAllianceImplementation.sol
@@ -4,52 +4,60 @@ import "./AllianceStorage.sol";
 import "./IAllianceImplementation.sol";
 import "./Election.sol";
 
-contract DefaultAllianceImplementation is AllianceStorage, IAllianceImplementation {
+contract DefaultAllianceImplementation is
+    AllianceStorage,
+    IAllianceImplementation
+{
     modifier onlyMember() {
         require(members[msg.sender].is_member, "");
         _;
     }
-    
+
     modifier onlyOwnElection() {
         require(elections[msg.sender] == address(0), "");
         _;
     }
-    
-    
-    function join(address val) public override returns(address) {
+
+    function join(address val) public override returns (address) {
         Election.Proposal[] memory proposals = new Election.Proposal[](2);
-        
+
         proposals[0].name = "Accept";
         proposals[0].description = "blablabla";
         proposals[0].won_action = member_add_action;
-        
+
         proposals[1].name = "Not accept";
         proposals[1].description = "blablabla";
         proposals[1].won_action = empty_action;
-        
-        Election election = new Election();
-        
+
+        Election election = new Election(30, 70, 30);
+
         candidates_for_membership[val] = address(election);
         elections[address(election)] = msg.sender;
-        
+
         return address(election);
     }
-    function exclude(address val) public override onlyMember returns(address) {
+
+    function exclude(address val) public override onlyMember returns (address) {
         return address(0);
     }
-    function leave() public override onlyMember returns(address) {
+
+    function leave() public override onlyMember returns (address) {
         return address(0);
     }
-    
-    function delegateTo(address val) public override onlyMember {
-    }
-    function undelegateFrom(address val) public override onlyMember {
-    }
-    
-    function updateImplementation(address val) public override onlyMember returns(address) {
+
+    function delegateTo(address val) public override onlyMember {}
+
+    function undelegateFrom(address val) public override onlyMember {}
+
+    function updateImplementation(address val)
+        public
+        override
+        onlyMember
+        returns (address)
+    {
         return address(0);
     }
-    
+
     function processVotingResult(uint256) public override onlyOwnElection {
         Election voting = Election(msg.sender);
         Election.Proposal memory won_proposal; // = voting.get_won_proposal();
--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,44 +1,162 @@
 pragma solidity >=0.5.0;
 
 import "./IAction.sol";
+import "./Alliance.sol";
 
-contract Election  {
-    
+contract Election {
     struct Voter {
-        int8 vote;
+        uint256 vote;
         bool discard;
+        bool voted;
+        uint256 prevVotePower;
     }
-    
+
     struct Proposal {
         string name;
         string description;
         IAction won_action;
         uint256 vote_count;
     }
-    
+
     address owner;
+    Alliance impl = Alliance(owner);
+
+    uint256 end_time;
+
     Proposal[] public proposals;
     mapping(address => Voter) public authorized_votes;
     mapping(address => Voter) public not_authorized_votes;
-    
+
     uint256 public discards;
-    
+
     uint256 public immutable failed_threshold;
     uint256 public immutable win_ratio;
     uint256 public immutable discard_threshold;
-    
-    constructor(uint256 f_th, uint256 wr, uint256 desc_th) public {
+
+    constructor(
+        uint256 f_th,
+        uint256 wr,
+        uint256 desc_th
+    ) public {
         owner = msg.sender;
-        
+        end_time = block.timestamp + (1440 * 1 minutes);
         failed_threshold = f_th;
         win_ratio = wr;
         discard_threshold = desc_th;
     }
 
-    function vote(uint8 id, uint256 power) public returns(uint256) {
-        return 0;
+    // power ??
+    function vote(uint256 id, uint256 power) public returns (uint256) {
+        require(block.timestamp < end_time, "Election is over.");
+        require(
+            id >= 0 && id <= proposals.length,
+            "Incorrect ID of the proposal."
+        );
+
+        if (checkVoterAuthorization()) {
+            if (authorized_votes[msg.sender].voted) {
+                proposals[authorized_votes[msg.sender].vote]
+                    .vote_count -= authorized_votes[msg.sender].prevVotePower;
+
+                // change to new id
+                authorized_votes[msg.sender].vote = id;
+
+                proposals[authorized_votes[msg.sender].vote]
+                    .vote_count += power;
+
+                authorized_votes[msg.sender].prevVotePower = power;
+            }
+            if (authorized_votes[msg.sender].discard) {
+                authorized_votes[msg.sender].discard = false;
+                authorized_votes[msg.sender].voted = true;
+                discards -= authorized_votes[msg.sender].prevVotePower;
+                authorized_votes[msg.sender].vote = id;
+
+                proposals[authorized_votes[msg.sender].vote]
+                    .vote_count += power;
+                authorized_votes[msg.sender].prevVotePower = power;
+            }
+            if (
+                !authorized_votes[msg.sender].discard &&
+                !authorized_votes[msg.sender].voted
+            ) {
+                authorized_votes[msg.sender].voted = true;
+                authorized_votes[msg.sender].vote = id;
+                proposals[authorized_votes[msg.sender].vote]
+                    .vote_count += power;
+                authorized_votes[msg.sender].prevVotePower = power;
+            }
+        } else {
+            if (not_authorized_votes[msg.sender].voted) {
+                proposals[not_authorized_votes[msg.sender].vote]
+                    .vote_count -= 1;
+
+                // change to new id
+                not_authorized_votes[msg.sender].vote = id;
+
+                proposals[not_authorized_votes[msg.sender].vote]
+                    .vote_count += 1;
+            }
+            if (not_authorized_votes[msg.sender].discard) {
+                not_authorized_votes[msg.sender].discard = false;
+                not_authorized_votes[msg.sender].voted = true;
+                discards -= 1;
+                not_authorized_votes[msg.sender].vote = id;
+
+                proposals[not_authorized_votes[msg.sender].vote]
+                    .vote_count += 1;
+            }
+            if (
+                !not_authorized_votes[msg.sender].discard &&
+                !not_authorized_votes[msg.sender].voted
+            ) {
+                not_authorized_votes[msg.sender].voted = true;
+                not_authorized_votes[msg.sender].vote = id;
+                proposals[authorized_votes[msg.sender].vote].vote_count += 1;
+            }
+        }
+        return id;
     }
-    
-    function discard() public {    
+
+    function discard(uint256 power) public {
+        require(block.timestamp < end_time, "Election is over.");
+        require(!authorized_votes[msg.sender].discard, "Already discarded");
+        require(!not_authorized_votes[msg.sender].discard, "Already discarded");
+
+        if (checkVoterAuthorization()) {
+            authorized_votes[msg.sender].discard = true;
+            if (authorized_votes[msg.sender].voted) {
+                proposals[authorized_votes[msg.sender].vote]
+                    .vote_count -= authorized_votes[msg.sender].prevVotePower;
+
+                // change to invalid proposal id
+                authorized_votes[msg.sender].vote = 0;
+                authorized_votes[msg.sender].voted = false;
+                authorized_votes[msg.sender].prevVotePower = power;
+            }
+            discards += power;
+        } else {
+            not_authorized_votes[msg.sender].discard = true;
+
+            // not_auth can't have delegates -> no previous power
+
+            if (not_authorized_votes[msg.sender].voted) {
+                proposals[not_authorized_votes[msg.sender].vote]
+                    .vote_count -= 1;
+
+                not_authorized_votes[msg.sender].vote = 0;
+                not_authorized_votes[msg.sender].voted = false;
+            }
+            discards += 1;
+        }
+    }
+
+    function checkVoterAuthorization() public returns (bool) {
+        bool member_status = impl.members(msg.sender);
+        if (member_status) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
